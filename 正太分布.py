c83c6625c12302cddb34533e7e04df0a77d011a8
PengShouChang <290064004@qq.com>
2018-05-06 18:34:03 +0800

    add 正太分布.py

--- 正太分布.py
+++ 正太分布.py
@@ -19,12 +19,12 @@ def inverfsampling(mu=0, sigma=1, size=1):
 
 if __name__ == '__main__':
     total = 100 #所分总金额
-    num = 500 #拆分红包
+    num = 500 #拆分红包.
     num_bins = 50
     average = float(total) / float(num) #平均数
     min = 0.01 #最小值
     total = total - min * num
-    sigma = 0.005 #标准差
+    sigma = 0.002 #标准差
     nums = []
     for i in range(0,num):
         result  = total
@@ -41,14 +41,14 @@ if __name__ == '__main__':
         elif i == (num -1):
             total = 0
         nums.append(round((min+result),2))
-        print("第%d个红包金额:%f" % ((i+1),round((min+result),2)))
-        print("剩余金额:%f" % ((min * (num - i -1)) if (i != num -1 and total == 0) else (total + (min * (num -i -1)))))
+        print("第%d个红包金额:%.2f" % ((i+1),round((min+result),2)))
+        print("剩余金额:%.2f" % ((min * (num - i -1)) if (i != num -1 and total == 0) else (total + (min * (num -i -1)))))
     print nums,sum(nums)
     n, bins, patches = plt.hist(nums, num_bins, normed=True, facecolor = 'blue', alpha = 0.5)
     y = mlab.normpdf(bins, average, sigma)
     plt.plot(bins, y, 'r--')
     plt.xlabel('Amount of money')
     plt.ylabel('Probability')
-    plt.title('Red envelopes: $\mu = %f$, $\sigma=%f$' % (round(average,2),round(sigma,2)))
+    plt.title('Red envelopes: $\mu = %.2f$, $\sigma=%.2f$' % (round(average,2),round(sigma,2)))
     plt.subplots_adjust(left = 0.15)
     plt.show()
